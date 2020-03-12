3dbfa8b9b85aee8e2ffb5bf1f31625b34353023b
Mandi Haase <haase1020@gmail.com>
2020-03-11 23:09:12 -0400

    add second router get function

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,21 +1,52 @@
 const express = require('express');
-
 const router = express.Router();
+const postRouter = require('../posts/postRouter');
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+    const user = req.body;
+    Users.insert(user)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+const { body } = req;
+Posts.insert(body)
+.then(post => {
+  res.status(200).json(post);
+})
+.catch(err => {
+  console.log(`Error: ${err}`);
+});
 });
 
-router.get('/', (req, res) => {
+router.get('/',validateUserId, (req, res) => {
   // do your magic!
+  Users.get(req.query)
+  .then(user => {
+    res.status(200).json(user);
+  })
+  .catch(err => {
+    console.log (`Error: ${err}`);
+  });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.params;
+  Users.getUserPosts(id)
+      .then(user => {
+          res.status(200).json(user);
+      })
+      .catch(err => {
+          console.log(`Error: ${err}`);
+      });
 });
 
 router.get('/:id/posts', (req, res) => {
