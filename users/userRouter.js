5f91f5f5e023afc50c955da589d5303fa66faf91
Mandi Haase <haase1020@gmail.com>
2020-03-11 23:31:56 -0400

    input validateUser function for custom middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -107,6 +107,13 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    return res.status(400).json({ message: "Missing user data" })
+  } else if (!req.body.name) {
+    return res.status(400).json({ message: "Missing required text field" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
